de20f02a9ce6dc66d5cd622dbbaec177ef804f0e
Ivan Zakharov <scotfarel@gmail.com>
2019-11-26 23:41:29 +0300

    Added using db api methods

--- actual assistant version/pixmap.py
+++ actual assistant version/pixmap.py
@@ -13,7 +13,7 @@ from PyQt5.QtGui import QFont, QPainter, QImage, QTextCursor
 
 from gui import *
 import sensor_recognition
-
+import database_api
 
 capturing = False
 frames_queue = queue.Queue()
@@ -56,22 +56,34 @@ class MyWin(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(1)
 
+    def set_empty_sensor_values(self):
+        self.ui.sensorRatedLoad.setText('')
+        self.ui.sensorWeight.setText('')
+        self.ui.sensorAccuracyClass.setText('')
+        self.ui.sensorMaterial.setText('')
+        self.ui.sensorInputImpedance.setText('')
+        self.ui.sensorOutputImpedances.setText('')
+
     def define_sensor_parameters(self):
+        self.set_empty_sensor_values()
+
         sensor_id = self.ui.sensorInfoInput.toPlainText()
         if sensor_id == '':
             self.ui.sensorTypePicture.setText('Введите идентификатор датчика')
             return
 
-        # go to DB, return parameters of current sensor
-        # sensor_parameters = get_sensor_parameters(id)
-        # sensor_name from db
-
-        sensor_rated_load = 1488
-        sensor_weight = 322
-        sensor_accuracy_class = 228
-        sensor_material = 1337
-        sensor_input_impedance = 420
-        sensor_output_impedance = 69
+        sensor_db = database_api.AssistantApp('', 'AssistantSensorsDatabase')
+        err, sensor_parameters = sensor_db.get_sensor_parameters(sensor_id)
+        if err:
+            self.ui.sensorTypePicture.setText(err)
+            return
+        else:
+            sensor_rated_load = sensor_parameters['SensorRatedLoad']
+            sensor_weight = sensor_parameters['SensorWeight']
+            sensor_accuracy_class = sensor_parameters['SensorAccuracyClass']
+            sensor_material = sensor_parameters['SensorMaterial']
+            sensor_input_impedance = sensor_parameters['SensorInputImpedance']
+            sensor_output_impedance = sensor_parameters['SensorOutputImpedance']
 
         self.ui.sensorRatedLoad.setText(str(sensor_rated_load))
         self.ui.sensorWeight.setText(str(sensor_weight))
@@ -87,7 +99,7 @@ class MyWin(QtWidgets.QMainWindow):
         if capturing:
             frame = frames_queue.get()
             # img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-            recognized_frame = sensor_recognition.find_displacement_of_centers(frame)
+            recognized_frame, _ = sensor_recognition.find_displacement_of_centers(frame)
             if type(recognized_frame) is not int:
                 # recognized_frame = cv2.cvtColor(recognized_frame, cv2.COLOR_GRAY2RGB)
                 height, width, channel = recognized_frame.shape
